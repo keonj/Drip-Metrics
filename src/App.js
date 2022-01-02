b1c3ef938a2b897930b15f7f86cfd769cc88389a
swan484 <swan484@uwo.ca>
2022-01-02 17:21:44 -0500

    set up basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import './App.css';
+
 function App() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
