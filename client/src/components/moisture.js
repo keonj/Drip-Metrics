f9904c3e7b45bfd8dace28f65ee9781f6a201eee
Keon Jin <dkjgoalie@gmail.com>
2022-01-11 14:25:55 -0500

    test

--- client/src/components/moisture.js
+++ client/src/components/moisture.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
-
+import ChartsEmbedSDK from '@mongodb-js/charts-embed-dom';
  
 export default function Moisture() {
 
  // This following section will display the form that takes the input from the user.
  // Put HTML here
- return ( 
-     <p>moisture something</p>
+ 
+ function myFunction() {
+
+    const sdk = new ChartsEmbedSDK({ baseUrl: "https://charts.mongodb.com/charts-dripdatabase-tkohi", showAttribution: false });
+    const chart = sdk.createChart({ chartId: "8fae5196-bd76-4776-b6b0-ae7931086753" });
+
+    chart.render(document.getElementById('chart'));
+    }
+
+ return (
+     <div>
+        <div ref="chart"></div>
+        <button onclick={myFunction}></button>
+     </div> 
  );
 }
